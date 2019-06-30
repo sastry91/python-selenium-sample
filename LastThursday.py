9a12f3492b75cd279e15303e64940e085f5072d7
Madhusudhan Sastry <sastry1991@gmail.com>
2019-06-30 20:38:12 +0530

    Updated last Thursday logic

--- LastThursday.py
+++ LastThursday.py
@@ -7,7 +7,17 @@ class MyLastThursday:
         # Create a datetime.date for the last day of the given month
         year = now.year;
         month = now.month;
+        computedDate = self.computeDate(year, month)
+        if computedDate > now :
+            print(computedDate)
+            return computedDate.strftime('%d%b%Y').upper()
+        else:
+            computedDate = self.computeDate(year, month+1)
+            print(computedDate)
+            return computedDate.strftime('%d%b%Y').upper()
 
+
+    def computeDate(self,year,month):
         daysInMonth = calendar.monthrange(year, month)[1]  # Returns (month, numberOfDaysInMonth)
         dt = datetime.date(year, month, daysInMonth)
 
@@ -15,5 +25,4 @@ class MyLastThursday:
         offset = 4 - dt.isoweekday()
         if offset > 0: offset -= 7  # Back up one week if necessary
         dt += datetime.timedelta(offset)  # dt is now date of last Th in month
-        print(dt.strftime('%d%b%Y').upper())
-        return dt.strftime('%d%b%Y').upper()
+        return dt;
